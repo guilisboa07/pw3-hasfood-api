3cd1c98cf15e00783fc44a1f45c35fa9302b18d4
guilisboa07 <guilhermy.lisboa05@gmail.com>
2022-03-28 08:35:22 -0300

    Criacao da entidade ItemCardapio

--- pw3-hasfood-api/src/main/java/br/com/guilhermylisboa/entity/ItemCardapio.java
+++ pw3-hasfood-api/src/main/java/br/com/guilhermylisboa/entity/ItemCardapio.java
@@ -1,5 +1,37 @@
 package br.com.guilhermylisboa.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import br.com.guilhermylisboa.enums.StatusEnum;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name="TBL_ITEM_CARDAPIO")
 public class ItemCardapio {
+
+@Id
+@Column(name = "ID_ITEM_CARDAPIO")
+private long idItemCardapio;
+
+@Column(name = "TX_NOME")
+private String nome;
+
+@Column(name = "TX_DESCRICAO")
+private String descricao;
+
+@Column(name = "VLR_ITEM")
+private float valorItem;
+
+@Column(name = "TX_STATUS")
+private StatusEnum Status;
+
 
 }
